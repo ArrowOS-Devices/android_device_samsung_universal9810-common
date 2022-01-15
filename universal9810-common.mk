88f5f7b502f7fd320aa294ebb738b438f500f26a
francoism90 <francoism90@users.noreply.github.com>
2022-01-15 10:23:43 +0100

    Add graphics

--- universal9810-common.mk
+++ universal9810-common.mk
@@ -71,6 +71,13 @@ PRODUCT_PACKAGES += \
     libsensorndkbridge \
     Snap
 
+# Display
+PRODUCT_PACKAGES += \
+    android.hardware.graphics.allocator@2.0-impl:64 \
+    android.hardware.graphics.allocator@2.0-service \
+    android.hardware.graphics.composer@2.2-service \
+    android.hardware.graphics.mapper@2.0-impl
+
 # DRM
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.3-service.clearkey
